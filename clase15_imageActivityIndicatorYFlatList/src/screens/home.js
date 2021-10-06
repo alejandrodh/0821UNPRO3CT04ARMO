a51d14d586b4187514e9c6844be6bbd73ecdba8b
alejandrodh <alejandro@digitalhouse.com>
2021-10-06 12:08:35 -0300

    Image, FlatList y ActivityIndicator.

---
+++ clase15_imageActivityIndicatorYFlatList/src/screens/home.js
@@ -0,0 +1,72 @@
+import React, {Component} from 'react';
+import Contador from '../components/Contador';
+import {Text,
+        TouchableOpacity,
+        View,
+        StyleSheet,
+        Image,
+        ActivityIndicator,
+        FlatList } from 'react-native';
+
+const profesores = [
+    {
+        id: 1,
+        nombre: 'Ale'
+    },
+    {
+        id:2,
+        nombre: 'Seba'
+    },
+    {
+        id:3,
+        nombre: 'Adri'
+    }
+]
+
+class Home extends Component{
+    constructor(){
+        super()
+        this.state={
+
+        }
+    }
+
+    render(){
+        return(
+            <View>
+                <Text style={styles.title}> Hola Mundo </Text>
+                <ActivityIndicator size='large' color='red'/>
+                <Image
+                    style={styles.imagen} 
+                    source={require('../../assets/avatar/cell.jpg')}
+                    resizeMode='contain'
+                />
+
+                <FlatList 
+                    data = {profesores}
+                    keyExtractor = { profesor => profesor.nombre}
+                    renderItem= {({item})=><Text>{item.nombre}</Text>}
+                />
+
+                <TouchableOpacity onPress={()=> console.log('Me clickearon')}>
+                    <Text>clickeame</Text>
+                </TouchableOpacity>
+                <Contador />
+            </View>
+        )
+    }
+}
+
+const styles = StyleSheet.create({
+    title: {
+        fontSize: 20,
+        fontWeight: 'bold',
+        textDecoration: 'underline',
+        textAlign: 'center',
+    },
+    imagen:{
+        height:250,
+    }
+})
+
+export default Home;
